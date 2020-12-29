6c89810003dd794e07c7e942633291b1e4becf24
Ziyang Li <ZL@auburn.edu>
2020-12-29 14:11:10 -0600

    board built and has a timer

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,25 @@ import React from 'react';
 import './App.css';
 import Board from './components/Board'
 function App() {
+  let width = 16;
+  let height = 10
+  let start_x = Math.floor(Math.random() * Math.floor(height));
+  let start_y = Math.floor(Math.random() * Math.floor(width));
+  let end_x = randomNumber(0, height-1, start_x);
+  let end_y = randomNumber(0, width-1, start_y);
   return (
     <div className="App">
       <header className="App-header">
-        <Board/>
+        <h2>BFS</h2>
+        <Board width={width} height={height} start_x={start_x} start_y={start_y} end_x={end_x} end_y={end_y}/>
       </header>
     </div>
   );
+}
+
+function randomNumber(max: number, min: number, except: number): number {
+    let num = Math.floor(Math.random() * (max - min +1)) + min;
+    return (num === except) ? randomNumber(max, min, except) : num;
 }
 
 export default App;
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,22 +1,67 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 
-export default function Board() {
-    let width: number = 30;
-    let height: number = 10;
+export default function Board(props: {width: number, height: number, start_x: number, start_y: number, end_x: number, end_y: number}) {
+
+    const [time, setTime] = useState(0);
+
+    useEffect(() => {
+    const interval = setInterval(() => setTime(time+1), 1000);
+    return () => {
+        clearInterval(interval);
+    };
+    }, [time]);
+
+    let width = props.width;
+    let height = props.height;
+    let start_x = props.start_x;
+    let start_y = props.start_y;
+    let end_x = props.end_x;
+    let end_y = props.end_y;
+
     let board = [];
     for(let i=0; i < height; i++) {
         let row = []
         for(let j = 0; j < width; j++) {
-            row.push('O')
+            if (i === start_x && j === start_y) {
+                row.push('S')
+            }
+            else if (i === end_x && j === end_y) {
+                row.push('E')
+            }
+            else {
+                row.push('O')
+            }
         }
         board.push(row)
     };
     return (
         <div>
+
+            <table>
             {board.map((x,i) => {
-                return <p key={i}>{x}</p>
+                return <tr key={i}>
+                    {x.map((v,j) => {
+                        if (v === 'O') {
+                            return <th key={i.toString() + j.toString()}>{v}</th>
+                        }
+                        else if (v === 'S') {
+                            return <th style={{color: "#ef4f4f"}} key={i.toString() + ' ' + j.toString()}>{v}</th>
+                        }
+                        else if (v === 'E') {
+                            return <th style={{color: "#11698e"}} key={i.toString() + ' ' + j.toString()}>{v}</th>
+                        }
+                        else if (v === "V") {
+                            return <th style={{color: "#e6e6e6"}} key={i.toString() + ' ' + j.toString()}>{v}</th>
+                        }
+                        else {
+                            return <th key={i.toString() + ' ' + j.toString()}>{v}</th>
+                        }
+                    })}
+                </tr>
             })}
+            </table>
+            <p>{time}</p>
         </div>
     )
 }
